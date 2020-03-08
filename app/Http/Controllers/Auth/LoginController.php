88af8d77825767e8af86880d85db8638c93f60cf
Soreka <rezafm1001@gmail.com>
2020-03-08 13:38:49 +0330

    inital commit

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Traits\MyAuthenticatesUsers;
 
 class LoginController extends Controller
 {
@@ -19,7 +19,7 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use MyAuthenticatesUsers;
 
     /**
      * Where to redirect users after login.
