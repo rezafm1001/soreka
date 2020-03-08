d82acbf4dd874e76e1dccf2c08b11017e8b493f9
Soreka <rezafm1001@gmail.com>
2020-03-08 11:39:42 +0330

    Initial commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+        $this->call(PermissionTableSeeder::class);
          $this->call(UsersTableSeeder::class);
-         $this->call(PermissionTableSeeder::class);
     }
 }
